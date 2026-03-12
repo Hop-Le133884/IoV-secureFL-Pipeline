9aaee642fc00689707a3786bd4916009933d47f9
Hop-Le133884 <hople133884@gmail.com>
2026-03-11 20:31:15 -0500

    update: adding weighted average for f1 score to calculate f1 from all the vehicles

--- src/federated/server.py
+++ src/federated/server.py
@@ -1,6 +1,16 @@
 #server.py
 import flwr as fl
 
+# Define how the server should average the F1 scores from the vehicles
+def weighted_average(metrics):
+    # Multiply each F1 score by the number of test examples the vehicle had
+    weighted_f1 = [num_examples * m["macro_f1"] for num_examples, m in metrics]
+    # Sum up all the examples from all vehicles
+    total_examples = sum(num_examples for num_examples, m in metrics)
+
+    # Return the weighted average F1 score
+    return {"macro_f1": sum(weighted_f1) / total_examples}
+
 def main():
     print("Starting Flower Central Server (AWS EMR Node)...")
 
@@ -12,6 +22,7 @@ def main():
         min_fit_clients=2,          # need at least 2 vehicles connected to start training
         min_evaluate_clients=2,     # need at least 2 vehciles to evaluate
         min_available_clients=2,    # wait until 2 vehicles check in
+        evaluate_metrics_aggregation_fn=weighted_average, # call weighted_average for macro f1 score average
     )
 
     # Start the server on port 8080
